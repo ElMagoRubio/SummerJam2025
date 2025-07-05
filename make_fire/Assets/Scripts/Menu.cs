9f7f94b27400c1bd5628bf5188f0b005555b7f7b
jrodsan <jesusrs4@hotmail.es>
2025-07-05 15:11:51 +0200

    menu opciones a medias

---
+++ make_fire/Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public abstract class Menu : MonoBehaviour
+{
+	public void Show()
+	{
+		gameObject.SetActive(true);
+	}
+
+	public void Hide()
+	{
+		gameObject.SetActive(false);
+	}
+}
