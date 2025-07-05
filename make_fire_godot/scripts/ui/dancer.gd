da64981aba4d215c048d7012398452a96c40fdcc
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-05 20:16:23 +0200

    dancer
    Creado bailarin

---
+++ make_fire_godot/scripts/ui/dancer.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var timer = $RotationTime
+@onready var sprite = $Sprite2D
+
+@onready var counter = false
+
+func _ready():
+	timer.wait_time = 0.2 #Modificar para seguir ritmo musica
+	timer.start()
+	timer.timeout.connect(_on_rotation_time_timeout)
+
+func _on_rotation_time_timeout() -> void:
+	counter = !counter
+	print(counter)
+	
+	if counter == false:
+		sprite.rotation += deg_to_rad(30)
+	else:
+		sprite.rotation -= deg_to_rad(30)
