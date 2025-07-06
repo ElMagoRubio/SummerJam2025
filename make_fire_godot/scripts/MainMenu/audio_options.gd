d52752d6ca334ac7a4d9f2b8cec4c6447c2e9b3c
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-06 16:23:43 +0200

    update menus

--- make_fire_godot/scripts/MainMenu/audio_options.gd
+++ make_fire_godot/scripts/MainMenu/audio_options.gd
@@ -5,4 +5,4 @@ extends Control
 func _on_back_pressed() -> void:
 	back_sound.play()
 	await get_tree().create_timer(0.2).timeout
-	get_tree().change_scene_to_file("res://scenes/MainMenu/menu_options.tscn")
+	get_tree().change_scene_to_file("res://scenes/MainMenu/menu.tscn")
