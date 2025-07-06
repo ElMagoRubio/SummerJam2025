cac60f2aa5b26ca2a6e39ab784a524ac68297514
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-06 16:00:11 +0200

    Update player

--- make_fire_godot/scripts/MainMenu/Options.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var back_sound := $VBoxContainer/Back/SoundPlayer
-
-func _on_back_pressed() -> void:
-	back_sound.play()
-	await get_tree().create_timer(0.2).timeout
-	get_tree().change_scene_to_file("res://scenes/MainMenu/menu.tscn")
