d57b5502ddfaf47babaed458b9550abcb89f1a97
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-06 05:56:20 +0200

    Update filesystem player

--- make_fire_godot/scripts/controllers/player.gd
+++ make_fire_godot/scripts/controllers/player.gd
@@ -7,18 +7,26 @@ extends Node2D
 
 var counter = false
 
-func _on_play_note_button_down() -> void:
-	counter = !counter
-	sprite.scale.x = abs(sprite.scale.x) * (-1 if counter else 1)
-
-	sprite.animation = "mantener"
+func _process(delta):
 	sprite.play()
+	await sprite.animation_finished
+	rotate_sprite()
+
+func _on_play_note_button_down() -> void:
+	sprite.animation = "maintain"
+	sprite.play()
+	await sprite.animation_finished
+	rotate_sprite()
 
 func _on_play_note_button_up() -> void:
-	sprite.animation = "golpe"
+	sprite.animation = "hit"
 	sprite.play()
 	
 	await sprite.animation_finished
 	
+	sprite.animation = "idle"
+	rotate_sprite()
+
+func rotate_sprite():
 	counter = !counter
 	sprite.scale.x = abs(sprite.scale.x) * (-1 if counter else 1)
