3d88321353109f5f27b81e0975154db2bb0f30bb
Pepealcrack <pepejaladoviedma@gmail.com>
2025-07-06 04:04:11 +0200

    Track clases

--- make_fire_godot/scripts/core/global_constants.gd
+++ make_fire_godot/scripts/core/global_constants.gd
@@ -4,3 +4,5 @@ const LEVEL_1_INSTRUMENT_0_MOD_1_MIDIJSON_PATH: String = "res://raw/tracks_json/
 const LEVEL_1_INSTRUMENT_1_MOD_1_MIDIJSON_PATH: String = "res://raw/tracks_json/level_1/instrument_0/lvl_1_inst_1_mod_1.json"
 const LEVEL_1_INSTRUMENT_2_MOD_1_MIDIJSON_PATH: String = "res://raw/tracks_json/level_1/instrument_0/lvl_1_inst_2_mod_1.json"
 const LEVEL_1_INSTRUMENT_3_MOD_1_MIDIJSON_PATH: String = "res://raw/tracks_json/level_1/instrument_0/lvl_1_inst_3_mod_1.json"
+
+const MAX_DIFFICULTY: int = 4
--- make_fire_godot/scripts/core/track_manager.gd
+++ make_fire_godot/scripts/core/track_manager.gd
@@ -3,32 +3,70 @@ class_name TrackManager
 
 var track_deserializer: TrackDeserializer = TrackDeserializer.new()
 
-var track_instrument_0: Track
-var track_instrument_1: Track
-var track_instrument_2: Track
-var track_instrument_3: Track
-
 var _actual_level: int
 var _actual_modifier: int
 
-func init_level(level: int, modifier: int):
+var _tracks: Array[Track] = []
+var _responders: Array[TrackResponder] = []
+var _validators: Array[TrackValidator] = []
+var _threads: Array[Thread] = []
+var _thread_data: Dictionary = {}
+
+func init_level(level: int, modifier: int) -> void:
 	_actual_level = level
 	_actual_modifier = modifier
 	_load_level_tracks()
-	print("INFO in TrackManager.init_level: Level loaded")
+	print("INFO: Level %d with modifier %d loaded" % [level, modifier])
 
 func _load_level_tracks() -> void:
-		track_instrument_0 = _load_track(_actual_level, 0, _actual_modifier)
-		print("INFO in TrackManager._load_level_tracks: Track 0 loaded")
-		track_instrument_1 = _load_track(_actual_level, 1, _actual_modifier)
-		print("INFO in TrackManager._load_level_tracks: Track 1 loaded")
-		track_instrument_2 = _load_track(_actual_level, 2, _actual_modifier)
-		print("INFO in TrackManager._load_level_tracks: Track 2 loaded")
-		track_instrument_3 = _load_track(_actual_level, 3, _actual_modifier)
-		print("INFO in TrackManager._load_level_tracks: Track 3 loaded")
+	for i in range(0, GlobalConstants.MAX_DIFFICULTY):
+		var track = track_deserializer.load_track_from_json(_actual_level, i, _actual_modifier)
+		if track != null:
+			_tracks.append(track)
+			print("Track %d loaded" % i)
 
-func _load_track(level: int, instrument: int, modifier: int) -> Track:
-	return track_deserializer.load_track_from_json(level, instrument, modifier)
+func start_track_execution() -> void:
+	for i in _tracks.size():
+		var track = _tracks[i]
+		var responder = TrackResponder.new()
+		var validator = TrackValidator.new()
 
-func get_tracks() -> Array[Track]:
-	return [track_instrument_0, track_instrument_1, track_instrument_2, track_instrument_3]
+		add_child(responder)
+		add_child(validator)
+
+		_responders.append(responder)
+		_validators.append(validator)
+
+		var thread := Thread.new()
+		_threads.append(thread)
+
+		# Llamamos al método y pasamos argumentos usando bind
+		var callable := Callable(self, "_thread_process_track").bind(track, responder, validator)
+		thread.start(callable)
+
+
+func _thread_process_track(track: Track, responder: TrackResponder, validator: TrackValidator) -> void:
+	for note in track._notes:
+		var wait_time := note._start_time - Time.get_ticks_msec() / 1000.0
+		if wait_time > 0:
+			await get_tree().create_timer(wait_time).timeout
+
+		await responder.emit_beat_events(note)
+
+		var success = validator.check_input(note)
+		if success:
+			call_deferred("_register_hit", track, note)
+		else:
+			call_deferred("_register_miss", track, note)
+
+
+func _register_hit(track: Track, note: Note) -> void:
+	print("✔ HIT on instrument %d at %.2f" % [track._instrument, note._original_time])
+
+func _register_miss(track: Track, note: Note) -> void:
+	print("✘ MISS on instrument %d at %.2f" % [track._instrument, note._original_time])
+
+func _exit_tree() -> void:
+	for thread in _threads:
+		if thread.is_alive():
+			thread.wait_to_finish()
